cd8d838d5b9f2e736c284deec7f5f525ad4652c0
ameed_f <ameed.lookup@gmail.com>
2019-01-14 21:44:22 +0200

    Examples for class creation

--- src/com/ameed/HelloWorld.java
+++ src/com/ameed/HelloWorld.java
@@ -1,7 +1,17 @@
+
 package com.ameed;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello" + " " + "1");
+		System.out.println("Hello" + " " + 1);
+		System.out.println("Hello" + " " + 1 + 2);
+		System.out.println("Hello" + " " + (1 + 2));
+		
+		int a = 1;
+		System.out.println(a);
+		System.out.println("Hello" + " " + "a");
+		System.out.println("Hello" + " " + a + 2);
 	}
 }
